c704d168e8bfdefa7395e8c3ce2d2e83f9043703
LKW07 <weiss.leander@steinweiss.de>
2025-01-07 13:01:46 +0100

    Create index.php, index.css

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/index.css">
+    <title>Homer - Smart Home</title>
+</head>
+<body>
+    <div class="header">
+        <h1>Homer</h1>
+    </div>
+    <div class="main"></div>
+    <div class="footer">
+        <p>&copy 2025 Leander Weiss</p>
+    </div>
+</body>
+</html>
